0665e7e7558adccadf441c0bde8818ad66f63b90
David Richardson <davidr@ebi.ac.uk>
2017-07-19 19:51:27 +0100

    Use HandleAfter[Save/Create] events to ensure predictable order of events

--- src/main/java/uk/ac/ebi/subs/api/handlers/CoreSubmittableEventHelper.java
+++ src/main/java/uk/ac/ebi/subs/api/handlers/CoreSubmittableEventHelper.java
@@ -1,10 +1,7 @@
 package uk.ac.ebi.subs.api.handlers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.rest.core.annotation.HandleAfterCreate;
-import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
-import org.springframework.data.rest.core.annotation.HandleBeforeSave;
-import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.data.rest.core.annotation.*;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.subs.api.services.SubmittableValidationDispatcher;
 import uk.ac.ebi.subs.api.services.ValidationResultService;
@@ -74,13 +71,13 @@ public class CoreSubmittableEventHelper {
 
     // Validation of updated submittables
 
-    @HandleBeforeSave
+    @HandleAfterSave
     public void validateOnSave(Sample sample) { submittableValidationDispatcher.validateUpdate(sample); }
 
-    @HandleBeforeSave
+    @HandleAfterSave
     public void validateOnSave(Study study) { submittableValidationDispatcher.validateUpdate(study); }
 
-    @HandleBeforeSave
+    @HandleAfterSave
     public void validateOnSave(Assay assay) {
         submittableValidationDispatcher.validateUpdate(assay);
     }
@@ -92,7 +89,7 @@ public class CoreSubmittableEventHelper {
         }
     }
 
-    @HandleBeforeSave
+    @HandleAfterSave
     public void beforeSave(StoredSubmittable storedSubmittable) {
         setTeamFromSubmission(storedSubmittable);
     }
