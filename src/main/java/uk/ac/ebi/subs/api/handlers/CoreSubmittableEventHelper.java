4172b36e2095b49975a0d17fc1c1cbacdb824187
David Richardson <davidr@ebi.ac.uk>
2017-07-21 13:54:35 +0100

    merge changes on event helper

--- src/main/java/uk/ac/ebi/subs/api/handlers/CoreSubmittableEventHelper.java
+++ src/main/java/uk/ac/ebi/subs/api/handlers/CoreSubmittableEventHelper.java
@@ -3,87 +3,77 @@ package uk.ac.ebi.subs.api.handlers;
 import org.springframework.data.rest.core.annotation.*;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.subs.api.services.SubmittableValidationDispatcher;
-import uk.ac.ebi.subs.repository.model.*;
-import uk.ac.ebi.subs.repository.repos.status.ProcessingStatusRepository;
+import uk.ac.ebi.subs.repository.model.Assay;
+import uk.ac.ebi.subs.repository.model.Sample;
+import uk.ac.ebi.subs.repository.model.StoredSubmittable;
+import uk.ac.ebi.subs.repository.model.Study;
 import uk.ac.ebi.subs.repository.services.SubmittableHelperService;
-import uk.ac.ebi.subs.validator.data.ValidationResult;
-import uk.ac.ebi.subs.validator.repository.ValidationResultRepository;
-
-import java.util.UUID;
 
 @Component
 @RepositoryEventHandler
 public class CoreSubmittableEventHelper {
 
+    public CoreSubmittableEventHelper(SubmittableHelperService submittableHelperService, SubmittableValidationDispatcher submittableValidationDispatcher) {
+        this.submittableHelperService = submittableHelperService;
+        this.submittableValidationDispatcher = submittableValidationDispatcher;
+    }
 
-    public CoreSubmittableEventHelper(
-                ProcessingStatusRepository processingStatusRepository,
-                SubmittableValidationDispatcher submittableValidationDispatcher,
-                ValidationResultRepository validationResultRepository) {
-            this.processingStatusRepository = processingStatusRepository;
-            this.submittableHelperService = submittableHelperService;
-            this.submittableValidationDispatcher = submittableValidationDispatcher;
-            this.validationResultRepository = validationResultRepository;
-        }
-
-        private ProcessingStatusRepository processingStatusRepository;
-        private SubmittableHelperService submittableHelperService;
-        private SubmittableValidationDispatcher submittableValidationDispatcher;
-        private ValidationResultRepository validationResultRepository;
+    private SubmittableHelperService submittableHelperService;
+    private SubmittableValidationDispatcher submittableValidationDispatcher;
 
 
-        /**
-         * Give submittables an ID and draft status on creation
-         *
-         * @param submittable
-         */
-        @HandleBeforeCreate
-        public void addDependentObjectsToSubmittable (StoredSubmittable submittable){
-            submittableHelperService.setupNewSubmittable(submittable);
-        }
+    /**
+     * Give submittables an ID and draft status on creation
+     *
+     * @param submittable
+     */
+    @HandleBeforeCreate
+    public void addDependentObjectsToSubmittable(StoredSubmittable submittable) {
+        submittableHelperService.setupNewSubmittable(submittable);
+    }
 
-        // Validation of created submittables
-        @HandleAfterCreate
-        public void validateOnCreate (Sample sample){
-            submittableValidationDispatcher.validateCreate(sample);
-        }
+    // Validation of created submittables
+    @HandleAfterCreate
+    public void validateOnCreate(Sample sample) {
+        submittableValidationDispatcher.validateCreate(sample);
+    }
 
-        @HandleAfterCreate
-        public void validateOnCreate (Study study){
-            submittableValidationDispatcher.validateCreate(study);
-        }
+    @HandleAfterCreate
+    public void validateOnCreate(Study study) {
+        submittableValidationDispatcher.validateCreate(study);
+    }
 
-        @HandleAfterCreate
-        public void validateOnCreate (Assay assay){
-            submittableValidationDispatcher.validateCreate(assay);
-        }
+    @HandleAfterCreate
+    public void validateOnCreate(Assay assay) {
+        submittableValidationDispatcher.validateCreate(assay);
+    }
 
-        // Validation of updated submittables
+    // Validation of updated submittables
 
-        @HandleAfterSave
-        public void validateOnSave (Sample sample){
-            submittableValidationDispatcher.validateUpdate(sample);
-        }
+    @HandleAfterSave
+    public void validateOnSave(Sample sample) {
+        submittableValidationDispatcher.validateUpdate(sample);
+    }
 
-        @HandleAfterSave
-        public void validateOnSave (Study study){
-            submittableValidationDispatcher.validateUpdate(study);
-        }
+    @HandleAfterSave
+    public void validateOnSave(Study study) {
+        submittableValidationDispatcher.validateUpdate(study);
+    }
 
-        @HandleAfterSave
-        public void validateOnSave (Assay assay){
-            submittableValidationDispatcher.validateUpdate(assay);
-        }
+    @HandleAfterSave
+    public void validateOnSave(Assay assay) {
+        submittableValidationDispatcher.validateUpdate(assay);
+    }
 
 
-        private void setTeamFromSubmission (StoredSubmittable submittable){
-            if (submittable.getSubmission() != null) {
-                submittable.setTeam(submittable.getSubmission().getTeam());
-            }
-        }
-
-        @HandleBeforeSave
-        public void beforeSave (StoredSubmittable storedSubmittable){
-            submittableHelperService.setTeamFromSubmission(storedSubmittable);
+    private void setTeamFromSubmission(StoredSubmittable submittable) {
+        if (submittable.getSubmission() != null) {
+            submittable.setTeam(submittable.getSubmission().getTeam());
         }
     }
+
+    @HandleBeforeSave
+    public void beforeSave(StoredSubmittable storedSubmittable) {
+        submittableHelperService.setTeamFromSubmission(storedSubmittable);
+    }
+}
