9b71b4289ccc46e26fa35353ccde48dd9d8a017b
Neil Goodgame <neilg@ebi.ac.uk>
2017-07-27 16:09:13 +0100

    ApiIntegrationTest that uses basic auth provided by the TestWebSecurityConfig

--- src/test/java/uk/ac/ebi/subs/api/BasicAuthApiIntegrationTest.java
+++ src/test/java/uk/ac/ebi/subs/api/BasicAuthApiIntegrationTest.java
@@ -1,7 +1,18 @@
-package uk.ac.ebi.subs.api.services;
+package uk.ac.ebi.subs.api;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import uk.ac.ebi.subs.ApiApplication;
 
 /**
  * Created by neilg on 27/07/2017.
  */
-public class BasicAuthApiIntegrationTest {
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("basic_auth")
+public class BasicAuthApiIntegrationTest extends ApiIntegrationTest {
+
 }
