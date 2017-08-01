fa2b73ef8aa017ce2e7045d8f929491e07d5712a
Karoly Erdos <karoly@ebi.ac.uk>
2017-08-01 16:57:45 +0100

    Set up CORS

---
+++ src/main/java/uk/ac/ebi/subs/api/config/CORSConfiguration.java
@@ -0,0 +1,25 @@
+package uk.ac.ebi.subs.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * This configuration file sets up CORS.
+ * 
+ * Created by karoly on 21/07/2017.
+ */
+@Configuration
+public class CORSConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**");
+            }
+        };
+    }
+}
