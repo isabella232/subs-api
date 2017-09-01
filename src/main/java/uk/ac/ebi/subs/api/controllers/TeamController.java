071ef3d7bacbb6ed51c130f379db7d15a66385d4
David Richardson <davidr@ebi.ac.uk>
2017-09-01 15:22:17 +0100

    relax checks on stub team controller
    we now create submissions from the team links, so we have to be able to
    use the team endpoint before submissions exist

--- src/main/java/uk/ac/ebi/subs/api/controllers/TeamController.java
+++ src/main/java/uk/ac/ebi/subs/api/controllers/TeamController.java
@@ -49,16 +49,9 @@ public class TeamController {
     @RequestMapping("/teams/{teamName}")
     @PreAuthorizeParamTeamName
     public Resource<Team> getTeam(@PathVariable @P("teamName") String teamName) {
-        //TODO this is a stub, we should make sure that the Teams are real and that the user is authorised
+
         Team d = new Team();
         d.setName(teamName);
-
-        Page<Submission> subsPage = submissionRepository.findByTeamName(teamName, new PageRequest(0, 1));
-
-        if (subsPage.getTotalElements() == 0) {
-            throw new ResourceNotFoundException();
-            //TODO temporary check until we have real team support
-        }
 
         Resource<Team> resource = new Resource<>(d);
 
