b99afdd8ddbdc2c9da669af53da061029da443e9
Neil Goodgame <neilg@ebi.ac.uk>
2017-10-05 16:34:53 +0100

    Returns a list of teams that are taken from the users AAP roles

--- src/main/java/uk/ac/ebi/subs/api/controllers/TeamController.java
+++ src/main/java/uk/ac/ebi/subs/api/controllers/TeamController.java
@@ -8,15 +8,26 @@ import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 import org.springframework.security.access.method.P;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.ac.ebi.subs.data.component.Team;
 import uk.ac.ebi.subs.repository.model.Submission;
 import uk.ac.ebi.subs.repository.repos.SubmissionRepository;
-import uk.ac.ebi.subs.repository.repos.SubmissionRepositoryCustom;
 import uk.ac.ebi.subs.repository.security.PreAuthorizeParamTeamName;
+
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.springframework.data.domain.PageImpl;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -26,22 +37,29 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 public class TeamController {
 
     private SubmissionRepository submissionRepository;
-    private PagedResourcesAssembler<Submission> pagedResourcesAssembler;
     private PagedResourcesAssembler<Team> teamPagedResourcesAssembler;
-    private SubmissionRepositoryCustom submissionRepositoryCustom;
     public TeamController(SubmissionRepository submissionRepository,
-                          SubmissionRepositoryCustom submissionRepositoryCustom,
-                          PagedResourcesAssembler<Submission> pagedResourcesAssembler,
                           PagedResourcesAssembler<Team> teamPagedResourcesAssembler) {
         this.submissionRepository = submissionRepository;
-        this.pagedResourcesAssembler = pagedResourcesAssembler;
         this.teamPagedResourcesAssembler = teamPagedResourcesAssembler;
-        this.submissionRepositoryCustom = submissionRepositoryCustom;
     }
 
     @RequestMapping("/teams")
-    public PagedResources<Resource<Team>> getTeams(Pageable pageable) {
-        Page<Team> teams = submissionRepositoryCustom.distinctTeams(pageable);
+    public Resources<Resource<Team>> getTeams(Pageable pageable) {
+
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        List<Team> teamList = new ArrayList<>();
+
+        if (authentication != null && authentication.getAuthorities().size() > 0) {
+            teamList = authentication.getAuthorities().stream().map(a -> {
+                Team team = new Team();
+                team.setName(a.getAuthority().toString().replaceFirst("ROLE_", ""));
+                return team;
+            }).collect(Collectors.toList());
+
+        }
+
+        final PageImpl<Team> teams = new PageImpl<>(teamList, pageable, authentication.getAuthorities().size());
         return teamPagedResourcesAssembler.toResource(teams);
     }
 
