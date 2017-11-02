38289ac5229fa50334a3b92426e301e9c445a5fa
David Richardson <davidr@ebi.ac.uk>
2017-11-02 14:49:16 +0000

    permit OPTIONS without authentication

--- src/test/java/uk/ac/ebi/subs/api/TestWebSecurityConfig.java
+++ src/test/java/uk/ac/ebi/subs/api/TestWebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.access.expression.SecurityExpressionOperations;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
