d75bc35e19ed71cfbee652516b576c53b5560a25
David Richardson <davidr@ebi.ac.uk>
2017-11-30 21:44:17 +0000

    use validation in api to ensure there's only one project in a submission

--- src/main/java/uk/ac/ebi/subs/api/validators/ProjectValidator.java
+++ src/main/java/uk/ac/ebi/subs/api/validators/ProjectValidator.java
@@ -8,6 +8,9 @@ import org.springframework.validation.Validator;
 import uk.ac.ebi.subs.repository.model.Project;
 import uk.ac.ebi.subs.repository.repos.submittables.ProjectRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Component
 public class ProjectValidator implements Validator {
 
@@ -25,6 +28,42 @@ public class ProjectValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         Project project = (Project) target;
+
         coreSubmittableValidationHelper.validate(project, projectRepository, errors);
+
+        if (project.getSubmission() == null || project.getSubmission().getId() == null) {
+            return; //it's not got a valid submission, there's no point doing the next bit
+        }
+
+        //there can only be one project in a submission
+
+        List<Project> projectsInSubmission = projectRepository.findBySubmissionId(
+                project.getSubmission().getId()
+        );
+
+        if (projectsInSubmission.isEmpty()) {
+            // there's no other projects in this submission, so this one will be the first
+            return;
+        }
+
+        boolean tooManyProjects = false;
+
+        if (project.getId() == null){
+            tooManyProjects = true;
+        }
+        else {
+            Optional<Project> optionalMatchedProject = projectsInSubmission.stream()
+                    .filter(storedProject -> storedProject.getId().equals(project.getId()))
+                    .findAny();
+
+            if (!optionalMatchedProject.isPresent()){
+                tooManyProjects = true;
+            }
+        }
+
+        if (tooManyProjects){
+            SubsApiErrors.already_exists.addError(errors);
+        }
+
     }
 }
