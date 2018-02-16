42cc57580422958c60532961b614ab51f432cac7
David Richardson <davidr@ebi.ac.uk>
2018-02-16 11:53:13 +0000

    parse sheet contents to JSON during validation

--- src/test/java/uk/ac/ebi/subs/api/SheetValidatorTest.java
+++ src/test/java/uk/ac/ebi/subs/api/SheetValidatorTest.java
@@ -11,17 +11,23 @@ import uk.ac.ebi.subs.api.services.OperationControlService;
 import uk.ac.ebi.subs.api.services.SheetService;
 import uk.ac.ebi.subs.api.validators.SheetValidator;
 import uk.ac.ebi.subs.api.validators.SubsApiErrors;
+import uk.ac.ebi.subs.data.status.SubmissionStatusEnum;
 import uk.ac.ebi.subs.repository.model.Submission;
+import uk.ac.ebi.subs.repository.model.SubmissionStatus;
 import uk.ac.ebi.subs.repository.model.sheets.Sheet;
+import uk.ac.ebi.subs.repository.model.templates.Capture;
 import uk.ac.ebi.subs.repository.model.templates.FieldCapture;
 import uk.ac.ebi.subs.repository.model.templates.Template;
 import uk.ac.ebi.subs.repository.repos.SheetRepository;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 public class SheetValidatorTest {
@@ -49,29 +55,31 @@ public class SheetValidatorTest {
 
         fakeSubmission = new Submission();
         fakeSubmission.setId("fs");
+        fakeSubmission.setSubmissionStatus(new SubmissionStatus());
+        fakeSubmission.getSubmissionStatus().setStatus(SubmissionStatusEnum.Draft);
     }
 
     @Test
     public void testNewGood() {
         Sheet sheet = exampleSheet();
 
-        given(sheetRepository.findOne("not-a-real-id")).willReturn(null);
-        given(sheetService.parse(sheet)).willReturn(jsonObjectStreamWithAlias());
-        given(operationControlService.isUpdateable(fakeSubmission)).willReturn(true);
-        given(errors.hasErrors()).willReturn(false);
+        when(sheetRepository.findOne("not-a-real-id")).thenReturn(null);
+        when(operationControlService.isUpdateable(fakeSubmission)).thenReturn(true);
+        when(errors.hasErrors()).thenReturn(false);
 
         validator.validate(sheet, errors);
 
+        assert (sheet.getRows().get(1).getDocument() != null);
+        assert (sheet.getRows().get(2).getDocument() != null);
     }
 
     @Test
     public void testNewSubmissionLocked() {
         Sheet sheet = exampleSheet();
 
-        given(sheetRepository.findOne("not-a-real-id")).willReturn(null);
-        given(sheetService.parse(sheet)).willReturn(jsonObjectStreamWithAlias());
-        given(operationControlService.isUpdateable(fakeSubmission)).willReturn(false);
-        given(errors.hasErrors()).willReturn(true);
+        when(sheetRepository.findOne("not-a-real-id")).thenReturn(null);
+        when(operationControlService.isUpdateable(fakeSubmission)).thenReturn(false);
+        when(errors.hasErrors()).thenReturn(true);
 
         validator.validate(sheet, errors);
 
@@ -121,15 +129,16 @@ public class SheetValidatorTest {
     public void testNewNoAlias() {
         Sheet sheet = exampleSheet();
 
-        given(sheetRepository.findOne("not-a-real-id")).willReturn(null);
-        given(sheetService.parse(sheet)).willReturn(jsonEmptyObjectStream());
+        sheet.addRow(new String[]{"", "bob", "bob"});
+
+        when(sheetRepository.findOne("not-a-real-id")).thenReturn(null);
 
         validator.validate(sheet, errors);
 
         verify(errors).rejectValue(
-                "rows",
-                SubsApiErrors.invalid.name(),
-                SubsApiErrors.invalid.name()
+                "rows[3]",
+                SubsApiErrors.missing_alias.name(),
+                SubsApiErrors.missing_alias.name()
         );
     }
 
@@ -152,9 +161,18 @@ public class SheetValidatorTest {
 
     private Sheet exampleSheet() {
         Sheet sheet = new Sheet();
-        sheet.setTemplate(Template.builder().targetType("thing").name("bob").build());
-        sheet.setHeaderRowIndex(1);
-        sheet.addRow(new String[]{"header1", "header2", "header3", "header4"});
+
+        Map<String, Capture> captureMap = new HashMap<>();
+
+        sheet.setTemplate(Template.builder()
+                .targetType("thing")
+                .name("bob")
+                .columnCaptures(captureMap)
+                .build());
+
+        sheet.setHeaderRowIndex(0);
+
+        sheet.addRow(new String[]{"alias", "header2", "header3", "header4"});
         sheet.addRow(new String[]{"a", "", "b", "c"});
         sheet.addRow(new String[]{"4", "", "5", "6"});
         sheet.setSubmission(fakeSubmission);
