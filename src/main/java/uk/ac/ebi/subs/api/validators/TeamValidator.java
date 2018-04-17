ab1adcd1125689bd77efcb4035f23ec1db1b4e7a
David Richardson <davidr@ebi.ac.uk>
2018-04-17 13:54:05 +0100

    validate that the team has a centre name

--- src/main/java/uk/ac/ebi/subs/api/validators/TeamValidator.java
+++ src/main/java/uk/ac/ebi/subs/api/validators/TeamValidator.java
@@ -14,6 +14,8 @@ public class TeamValidator implements Validator {
     @Getter
     private String expectedTeamNamePrefix;
 
+    private static final String[] expectedProfileAttributes = new String[]{"centre name"};
+
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -23,14 +25,25 @@ public class TeamValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         Team team = (Team) target;
-        SubsApiErrors.rejectIfEmptyOrWhitespace(errors,"name");
+        SubsApiErrors.rejectIfEmptyOrWhitespace(errors, "name");
 
-        if (team.getName() == null || team.getName().isEmpty()){
+        if (team.getName() == null || team.getName().isEmpty()) {
             return;
         }
 
-        if (!team.getName().startsWith(expectedTeamNamePrefix)){
-            SubsApiErrors.not_a_subs_team.addError(errors,"name");
+        if (!team.getName().startsWith(expectedTeamNamePrefix)) {
+            SubsApiErrors.not_a_subs_team.addError(errors, "name");
+        }
+
+        for (String expectedProfileAttributeKey : expectedProfileAttributes) {
+            if (
+                    !team.getProfile().containsKey(expectedProfileAttributeKey) ||
+                            team.getProfile().get(expectedProfileAttributeKey).isEmpty()) {
+                SubsApiErrors.missing_profile_attribute.addError(
+                        errors,
+                        "profile['" + expectedProfileAttributeKey + "']"
+                );
+            }
         }
     }
 }
