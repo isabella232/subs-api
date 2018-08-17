2b0548a48ee6cf5be4f4e72da81224424777825f
David Richardson <davidr@ebi.ac.uk>
2018-08-17 13:49:44 +0100

    open up reading of data descriptors

--- src/main/java/uk/ac/ebi/subs/api/config/SubsAAPWebSecurityConfig.java
+++ src/main/java/uk/ac/ebi/subs/api/config/SubsAAPWebSecurityConfig.java
@@ -67,6 +67,9 @@ public class SubsAAPWebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/health/summary").permitAll()
                 .antMatchers(HttpMethod.GET,"/uiSupportItems/**").permitAll()
                 .antMatchers(HttpMethod.GET,"/templates/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/submissionPlans/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/dataTypes/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/checklists/**").permitAll()
                 .mvcMatchers(HttpMethod.OPTIONS,"/**").permitAll()
                 .mvcMatchers(HttpMethod.HEAD,"/**").permitAll()
                 .anyRequest().authenticated();
