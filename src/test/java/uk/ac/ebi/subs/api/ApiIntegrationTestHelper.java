70311e2aceb4b4a4a21797a9ea0eb1ede1231a01
David Richardson <davidr@ebi.ac.uk>
2018-09-24 15:14:30 +0100

    id change for data type

--- src/test/java/uk/ac/ebi/subs/api/ApiIntegrationTestHelper.java
+++ src/test/java/uk/ac/ebi/subs/api/ApiIntegrationTestHelper.java
@@ -149,13 +149,13 @@ public class ApiIntegrationTestHelper {
         String submissionLocation = submissionResponse.getHeaders().getFirst("Location");
         Map<String, String> submissionRels = relsFromPayload(submissionResponse.getBody().getObject());
         Map<String,String> submissionContentsRels = relsFromUri(submissionRels.get("contents"));
-        assertThat(submissionContentsRels.get("sequencingStudies:create"), notNullValue());
+        assertThat(submissionContentsRels.get("enaStudies:create"), notNullValue());
 
         List<Study> testStudies = Helpers.generateTestClientStudies(2);
         //add samples to the submission
         for (Study study : testStudies) {
 
-            HttpResponse<JsonNode> studyResponse = Unirest.post(submissionContentsRels.get("sequencingStudies:create"))
+            HttpResponse<JsonNode> studyResponse = Unirest.post(submissionContentsRels.get("enaStudies:create"))
                     .headers(postHeaders)
                     .body(study)
                     .asJson();
