d45e1bb40cc1cd0f73ce52adb4ef405a2a63649a
David Richardson <davidr@ebi.ac.uk>
2018-10-02 15:47:23 +0100

    tidy imports

--- src/main/java/uk/ac/ebi/subs/api/config/RestRepositoryConfig.java
+++ src/main/java/uk/ac/ebi/subs/api/config/RestRepositoryConfig.java
@@ -1,25 +1,13 @@
 package uk.ac.ebi.subs.api.config;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.support.ConfigurableConversionService;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
 import uk.ac.ebi.tsc.aap.client.model.User;
-
-import java.util.List;
 
 @Configuration
 @EnableMongoAuditing(auditorAwareRef = "auditorProvider")
