644c69f3e7ee7c92c7c3470a8720b7c3895170b3
Karoly Erdos <karoly@ebi.ac.uk>
2018-10-08 16:38:04 +0100

    Catch AAPException and log it and do not throw it to the user

--- src/main/java/uk/ac/ebi/subs/api/services/UserTeamService.java
+++ src/main/java/uk/ac/ebi/subs/api/services/UserTeamService.java
@@ -2,6 +2,7 @@ package uk.ac.ebi.subs.api.services;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.ResourceAccessException;
 import uk.ac.ebi.subs.data.component.Team;
+import uk.ac.ebi.tsc.aap.client.exception.AAPException;
 import uk.ac.ebi.tsc.aap.client.model.Domain;
 import uk.ac.ebi.tsc.aap.client.model.Profile;
 import uk.ac.ebi.tsc.aap.client.repo.DomainService;
@@ -18,6 +20,7 @@ import uk.ac.ebi.tsc.aap.client.repo.ProfileService;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -29,6 +32,7 @@ public class UserTeamService {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Value("${usi.teamName.prefix}")
+    @Setter
     private String teamNamePrefix;
 
     @NonNull
@@ -44,6 +48,7 @@ public class UserTeamService {
         List<Team> teams = domains.stream()
                 .filter(d -> d.getDomainName().startsWith(teamNamePrefix))
                 .map(d -> domainToTeam(d, userToken))
+                .filter(Objects::nonNull)
                 .collect(Collectors.toList());
 
         return teams;
@@ -58,7 +63,14 @@ public class UserTeamService {
             t.setProfile(p.getAttributes());
         } catch (ResourceAccessException e) {
             logger.warn("failed to get profile for domain", e);
+        } catch (AAPException aapException) {
+            // We just log this error, because we don't want to throw this error to the user
+            // This could happen because the user's added a new domain and forgot to refresh its token
+            logger.warn("Failed to get profile information for this domain: {}. The user's token might be outdated and needs to be refreshed.",
+                    d.getDomainName());
+            return null;
         }
+
         return t;
     }
 
