cf837870e54fe9316401ba1fa6145239f708eeeb
Karoly Erdos <karoly@ebi.ac.uk>
2018-10-23 09:51:04 +0100

    Add self link to the resource

--- src/main/java/uk/ac/ebi/subs/api/controllers/SubmissionBlockersSummaryController.java
+++ src/main/java/uk/ac/ebi/subs/api/controllers/SubmissionBlockersSummaryController.java
@@ -1,15 +1,17 @@
 package uk.ac.ebi.subs.api.controllers;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
 import org.springframework.security.access.method.P;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import uk.ac.ebi.subs.data.fileupload.FileStatus;
 import uk.ac.ebi.subs.repository.model.ProcessingStatus;
-import uk.ac.ebi.subs.repository.model.fileupload.File;
 import uk.ac.ebi.subs.repository.repos.fileupload.FileRepository;
 import uk.ac.ebi.subs.repository.repos.status.ProcessingStatusRepository;
 import uk.ac.ebi.subs.validator.data.ValidationResult;
@@ -19,7 +21,9 @@ import uk.ac.ebi.subs.validator.repository.ValidatorResultRepositoryCustom;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 @RequiredArgsConstructor
@@ -35,43 +39,54 @@ public class SubmissionBlockersSummaryController {
     private ProcessingStatusRepository processingStatusRepository;
 
     @GetMapping(value = "/submissions/{submissionId}/submissionBlockersSummary")
-    public SubmissionIssuesSummary getSubmissionContentsIssuesSummary(@PathVariable @P("submissionId") String submissionId) {
-        SubmissionIssuesSummary submissionIssuesSummary = new SubmissionIssuesSummary();
+    public Resource<SubmissionBlockersSummary> getSubmissionContentsIssuesSummary(@PathVariable @P("submissionId") String submissionId) {
+        SubmissionBlockersSummary submissionBlockersSummary = new SubmissionBlockersSummary();
 
-        getFileIssues(submissionId, submissionIssuesSummary);
-        getMetadataIssues(submissionId, submissionIssuesSummary);
-        checkSubmissionEmptiness(submissionId, submissionIssuesSummary);
-        checkPendingValidationResults(submissionId, submissionIssuesSummary);
+        getFileIssues(submissionId, submissionBlockersSummary);
+        getMetadataIssues(submissionId, submissionBlockersSummary);
+        checkSubmissionEmptiness(submissionId, submissionBlockersSummary);
+        checkPendingValidationResults(submissionId, submissionBlockersSummary);
 
-        return submissionIssuesSummary;
+        Resource<SubmissionBlockersSummary> submissionIssuesSummaryResource = new Resource<>(submissionBlockersSummary);
+
+        Link selfLink = linkTo(methodOn(
+                this.getClass()).getSubmissionContentsIssuesSummary(submissionId)
+        ).withSelfRel();
+
+        submissionIssuesSummaryResource.add(selfLink);
+
+        return submissionIssuesSummaryResource;
     }
 
-    private void getFileIssues(String submissionId, SubmissionIssuesSummary submissionIssuesSummary) {
-        submissionIssuesSummary.notReadyFileCount = fileRepository.countBySubmissionIdAndStatusNot(submissionId, FileStatus.READY_FOR_ARCHIVE);
+
+
+    private void getFileIssues(String submissionId, SubmissionBlockersSummary submissionBlockersSummary) {
+        submissionBlockersSummary.notReadyFileCount = fileRepository.countBySubmissionIdAndStatusNot(submissionId, FileStatus.READY_FOR_ARCHIVE);
     }
 
-    private void getMetadataIssues(String submissionId, SubmissionIssuesSummary submissionIssuesSummary) {
-        submissionIssuesSummary.setValidationIssuesPerDataTypeId(
+    private void getMetadataIssues(String submissionId, SubmissionBlockersSummary submissionBlockersSummary) {
+        submissionBlockersSummary.setValidationIssuesPerDataTypeId(
                 validatorResultRepositoryCustom.validationIssuesPerDataTypeId(submissionId));
     }
 
-    private void checkSubmissionEmptiness(String submissionId, SubmissionIssuesSummary submissionIssuesSummary) {
+    private void checkSubmissionEmptiness(String submissionId, SubmissionBlockersSummary submissionBlockersSummary) {
         List<ValidationResult> validationResults = validationResultRepository.findAllBySubmissionId(submissionId);
         List<ProcessingStatus> processingStatuses = processingStatusRepository.findBySubmissionId(submissionId);
 
         if (validationResults.size() == 0 || processingStatuses.size() == 0) {
-            submissionIssuesSummary.setEmptySubmission(true);
+            submissionBlockersSummary.setEmptySubmission(true);
         }
     }
 
-    private void checkPendingValidationResults(String submissionId, SubmissionIssuesSummary submissionIssuesSummary) {
-        submissionIssuesSummary.anyPendingValidationResult =
+    private void checkPendingValidationResults(String submissionId, SubmissionBlockersSummary submissionBlockersSummary) {
+        submissionBlockersSummary.anyPendingValidationResult =
                 validationResultRepository.countBySubmissionIdAndValidationStatusIs(submissionId, GlobalValidationStatus.Pending) > 0;
     }
 
     @Data
-    class SubmissionIssuesSummary {
+    class SubmissionBlockersSummary {
         long notReadyFileCount;
+        @JsonInclude(content= JsonInclude.Include.NON_EMPTY)
         Map<String, Integer> validationIssuesPerDataTypeId;
         boolean emptySubmission;
         boolean anyPendingValidationResult;
